36754fcfc750a0b53468cd557582a43b3d7b6dcd
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-25 21:59:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["orange", "strawberry", "grape","peach"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["wow", "I", "am", "really", "learning"]
+    array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop("")
 end
 
 def remove_element_from_start_of_array(array)
